bc53e955d688dd613455dd030ad2824363841e05
Felix Bertram <felix@bertram-family.com>
2018-11-06 16:11:19 -0800

    minor refactoring

--- SimulatorEngine/IndicatorsVolatility.cs
+++ SimulatorEngine/IndicatorsVolatility.cs
@@ -31,7 +31,7 @@ namespace TuringTrader.Simulator
         /// <param name="series">input time series</param>
         /// <param name="n">length</param>
         /// <returns>standard deviation as time series</returns>
-        public static ITimeSeries<double> StandardDeviation(this ITimeSeries<double> series, int n)
+        public static ITimeSeries<double> StandardDeviation(this ITimeSeries<double> series, int n = 10)
         {
             // see https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
 
@@ -60,12 +60,70 @@ namespace TuringTrader.Simulator
                         ? (sum2 - sum * sum / num) / (num - 1)
                         : 0.0;
 
-                    return Math.Sqrt(252.0 * variance);
+                    return Math.Sqrt(variance);
                 },
                 0.0,
                 series.GetHashCode(), n);
         }
         #endregion
+        #region public static ITimeSeries<double> FastStandardDeviation(this ITimeSeries<double> series, int n)
+        /// <summary>
+        /// Calculate standard deviation, based on exponentially weighted filters. This is an
+        /// incremental calculation, based on Tony Finch, which is very fast and efficient.
+        /// </summary>
+        /// <param name="series">input time series</param>
+        /// <param name="n">filtering length</param>
+        /// <returns>variance as time series</returns>
+        public static ITimeSeries<double> FastStandardDeviation(this ITimeSeries<double> series, int n = 10)
+        {
+            var functor = Cache<FunctorStandardDeviation>.GetData(
+                    Cache.UniqueId(series.GetHashCode(), n),
+                    () => new FunctorStandardDeviation(series, n));
+
+            functor.Calc();
+
+            return functor;
+        }
+
+        private class FunctorStandardDeviation : TimeSeries<double>
+        {
+            public readonly ITimeSeries<double> Series;
+            public readonly int N;
+
+            private readonly double _alpha;
+            private double _average;
+            private double _variance;
+
+            public FunctorStandardDeviation(ITimeSeries<double> series, int n)
+            {
+                Series = series;
+                N = Math.Max(2, n);
+                _alpha = 2.0 / (N + 1.0);
+            }
+
+            public void Calc()
+            {
+                try
+                {
+                    // calculate exponentially-weighted mean and variance
+                    // see Tony Finch, Incremental calculation of mean and variance
+                    double diff = Series[0] - _average;
+                    double incr = _alpha * diff;
+                    _average = _average + incr;
+                    _variance = (1.0 - _alpha) * (_variance + diff * incr);
+
+                    Value = Math.Sqrt(_variance);
+                }
+                catch (Exception)
+                {
+                    // we get here when we access bars too far in the past
+                    _average = Series[0];
+                    Value = 0.0;
+                }
+            }
+        }
+        #endregion
+
         #region public static ITimeSeries<double> Volatility(this ITimeSeries<double> series, int n)
         /// <summary>
         /// Calculate historical volatility.
@@ -73,7 +131,7 @@ namespace TuringTrader.Simulator
         /// <param name="series">input time series</param>
         /// <param name="n">length</param>
         /// <returns>volatility as time series</returns>
-        public static ITimeSeries<double> Volatility(this ITimeSeries<double> series, int n)
+        public static ITimeSeries<double> Volatility(this ITimeSeries<double> series, int n = 10)
         {
             return series.LogReturn().StandardDeviation(n);
         }
@@ -93,66 +151,13 @@ namespace TuringTrader.Simulator
                     double hi = series.Highest(n)[0];
                     double lo = series.Lowest(n)[0];
 
-                    return 0.63 * Math.Sqrt(252.0 / n) * Math.Log(hi / lo);
+                    return 0.80 * Math.Sqrt(1.0 / n) * Math.Log(hi / lo);
                 },
                 0.0,
                 series.GetHashCode(), n);
         }
         #endregion
-        #region public static ITimeSeries<double> FastVariance(this ITimeSeries<double> series, int n)
-        /// <summary>
-        /// Calculate variance, based on exponentially weighted filters. This is an
-        /// incremental calculation, based on Tony Finch, which is very fast and efficient.
-        /// </summary>
-        /// <param name="series">input time series</param>
-        /// <param name="n">filtering length</param>
-        /// <returns>variance as time series</returns>
-        public static ITimeSeries<double> FastVariance(this ITimeSeries<double> series, int n)
-        {
-            var functor = Cache<FunctorFastVariance>.GetData(
-                    Cache.UniqueId(series.GetHashCode(), n),
-                    () => new FunctorFastVariance(series, n));
 
-            functor.Calc();
-
-            return functor;
-        }
-
-        private class FunctorFastVariance : TimeSeries<double>
-        {
-            public ITimeSeries<double> Series;
-            public int N;
-
-            private double _alpha;
-            private double _average;
-
-            public FunctorFastVariance(ITimeSeries<double> series, int n)
-            {
-                Series = series;
-                N = Math.Max(2, n);
-                _alpha = 2.0 / (N + 1.0);
-            }
-
-            public void Calc()
-            {
-                try
-                {
-                    // calculate exponentially-weighted mean and variance
-                    // see Tony Finch, Incremental calculation of mean and variance
-                    double diff = Series[0] - _average;
-                    double incr = _alpha * diff;
-                    _average = _average + incr;
-                    Value = (1.0 - _alpha) * (this[0] + diff * incr);
-                }
-                catch (Exception)
-                {
-                    // we get here when we access bars too far in the past
-                    _average = Series[0];
-                    Value = 0.0;
-                }
-            }
-        }
-        #endregion
         #region public static ITimeSeries<double> TrueRange(this Instrument series)
         /// <summary>
         /// Calculate True Range, non averaged, as described here:
