afbccfbdfb9c7f945a87dd404845d562ef5d189f
Felix Bertram <felix@bertram-family.com>
2019-07-16 11:37:26 -0700

    clean up license/ copyright headers.

--- Templates/SimpleChart.r
+++ Templates/SimpleChart.r
@@ -3,6 +3,21 @@
 # Name:        SimpleChart.r
 # Description: Template to create simple TuringTrader charts.
 # History:     2018xi13, FUB, created
+# Copyright:   (c) 2011-2019, Bertram Solutions LLC
+#              https://www.bertram.solutions
+# License:     This file is part of TuringTrader, an open-source backtesting
+#              engine/ market simulator.
+#              TuringTrader is free software: you can redistribute it and/or 
+#              modify it under the terms of the GNU Affero General Public 
+#              License as published by the Free Software Foundation, either 
+#              version 3 of the License, or (at your option) any later version.
+#              TuringTrader is distributed in the hope that it will be useful,
+#              but WITHOUT ANY WARRANTY; without even the implied warranty of
+#              MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+#              GNU Affero General Public License for more details.
+#              You should have received a copy of the GNU Affero General Public
+#              License along with TuringTrader. If not, see 
+#              https://www.gnu.org/licenses/agpl-3.0.
 #===============================================================================
 
 #-------------------------------------------------------------------------------
