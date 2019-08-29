9c4ad65d74e07714932bf4554bc2a1616d52c35e
Felix Bertram <felix@turingtrader.org>
2019-08-29 15:06:07 -0700

    add script for one-click build. update to latest MSVC preview.

---
+++ ONE_CLICK_BUILD.bat
@@ -0,0 +1,6 @@
+PATH=%PATH%;"C:\Program Files (x86)\Microsoft Visual Studio\2019\Preview\Common7\IDE"
+devenv TuringTrader.sln /Clean "Release|x64"
+devenv TuringTrader.sln /Build "Release|x64" /Project TuringTrader
+devenv TuringTrader.sln /Build "Release|x64" /Project BooksAndPubs
+dotnet publish TuringTrader\TuringTrader.csproj /p:PublishProfile=FolderProfile
+devenv TuringTrader.sln /Build "Release|x64" /Project TuringTrader.Setup
