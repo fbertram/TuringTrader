dc95e4fa3e3b6ee68a2ab96d22340e697934ed26
Felix Bertram <felix@turingtrader.org>
2020-03-26 17:27:07 -0700

    experiment w/ monthly bars. clean up code.

--- BooksAndPubs/Faber_IvyPortfolio.cs
+++ BooksAndPubs/Faber_IvyPortfolio.cs
@@ -22,6 +22,18 @@
 //              https://www.gnu.org/licenses/agpl-3.0.
 //==============================================================================
 
+// NOTE
+// it is not exactly clear, how Faber imagined this strategy implementation.
+// we especially have the following open questions:
+// - how are 1, 3, 6-month momentum calculated? daily or monthly bars?
+// - how is the 10-month SMA calculated? on daily or monthly bars?
+// - how is the momentum score calculated? simple average of 1, 3, 6-month, 
+//   or are they annualized first?
+
+// further implementation details taken from
+// http://www.scottsinvestments.com/2012/01/27/new-ivy-portfolio-tool/
+// https://docs.google.com/spreadsheets/d/1DbLtigPIUy8-dgA_v9FN7rVM2_5BD_H2SMgHoYCW8Sg/edit#gid=971644476
+
 #region libraries
 using System;
 using System.Collections.Generic;
@@ -34,7 +46,8 @@ using TuringTrader.Algorithms.Glue;
 
 namespace TuringTrader.BooksAndPubs
 {
-    public abstract class Faber_IvyPortfolio : Algorithm
+    #region Ivy Portfolio Core
+    public abstract class Faber_IvyPortfolio_Core : Algorithm
     {
         #region inputs
         protected struct AssetClass
@@ -53,7 +66,7 @@ namespace TuringTrader.BooksAndPubs
         private AllocationTracker _alloc = new AllocationTracker();
         #endregion
         #region ctor
-        public Faber_IvyPortfolio()
+        public Faber_IvyPortfolio_Core()
         {
             _plotter = new Plotter(this);
         }
@@ -71,13 +84,13 @@ namespace TuringTrader.BooksAndPubs
             Deposit(Globals.INITIAL_CAPITAL);
             CommissionPerShare = Globals.COMMISSION; // Faber does not consider commissions
 
-            var assets = ASSET_CLASSES
+            var ASSETS = ASSET_CLASSES
                 .SelectMany(c => c.assets)
                 .Distinct()
                 .ToList();
 
-            AddDataSource(BENCHMARK);
-            AddDataSources(assets);
+            var benchmark = AddDataSource(BENCHMARK);
+            var assets = AddDataSources(ASSETS);
 
             //========== simulation loop ==========
 
@@ -91,35 +104,36 @@ namespace TuringTrader.BooksAndPubs
 
                 // skip if there are any missing instruments,
                 // we want to make sure our strategy has all instruemnts available
-                if (!HasInstruments(assets) || !HasInstrument(BENCHMARK))
+                if (!HasInstruments(assets) || !HasInstrument(benchmark))
                     continue;
-
-                // create empty structure for instrument weights
-                Dictionary<Instrument, double> instrumentWeights = assets
-                    .ToDictionary(nick => FindInstrument(nick), nick => 0.0);
-
-                // loop through all asset classes
-                foreach (AssetClass assetClass in ASSET_CLASSES)
-                {
-                    List<Instrument> assetClassInstruments = assetClass.assets
-                        .Select(n => FindInstrument(n))
-                        .ToList();
-
-                    var bestInstruments = assetClassInstruments
-                        .OrderByDescending(i => instrumentMomentum[i])
-                        .Take(assetClass.numpicks);
-
-                    foreach (Instrument bestInstrument in bestInstruments)
-                        instrumentWeights[bestInstrument] += assetClass.weight / assetClass.numpicks;
-                }
 
                 // execute trades once per month
                 if (SimTime[0].Month != NextSimTime.Month)
                 {
+                    // create empty structure for instrument weights
+                    Dictionary<Instrument, double> instrumentWeights = assets
+                        .ToDictionary(ds => ds.Instrument, ds => 0.0);
+
+                    // loop through all asset classes and accumulate asset weights
+                    foreach (AssetClass assetClass in ASSET_CLASSES)
+                    {
+                        List<Instrument> assetClassInstruments = assetClass.assets
+                            .Select(n => FindInstrument(n))
+                            .ToList();
+
+                        var bestInstruments = assetClassInstruments
+                            .OrderByDescending(i => instrumentMomentum[i])
+                            .Take(assetClass.numpicks);
+
+                        foreach (Instrument bestInstrument in bestInstruments)
+                            instrumentWeights[bestInstrument] += assetClass.weight / assetClass.numpicks;
+                    }
+
                     double totalWeight = ASSET_CLASSES
                         .Sum(a => a.weight);
                     double equityUnit = NetAssetValue[0] / totalWeight;
 
+                    // create orders
                     _alloc.LastUpdate = SimTime[0];
                     string message = string.Format("{0:MM/dd/yyyy}: ", SimTime[0]);
                     foreach (var i in instrumentWeights.Keys)
@@ -139,7 +153,7 @@ namespace TuringTrader.BooksAndPubs
                         }
                     }
 
-                    if (TradingDays > 0 && !IsOptimizing && (EndTime - SimTime[0]).TotalDays < 31)
+                    if (TradingDays > 0 && !IsOptimizing && (EndTime - SimTime[0]).TotalDays < 63)
                         Output.WriteLine(message);
                 }
 
@@ -147,7 +161,7 @@ namespace TuringTrader.BooksAndPubs
                 if (TradingDays > 0)
                 {
                     _plotter.AddNavAndBenchmark(this, FindInstrument(BENCHMARK));
-                    _plotter.AddStrategyHoldings(this, assets.Select(nick => FindInstrument(nick)));
+                    _plotter.AddStrategyHoldings(this, ASSETS.Select(nick => FindInstrument(nick)));
                 }
             }
 
@@ -173,59 +187,114 @@ namespace TuringTrader.BooksAndPubs
         }
         #endregion
     }
+    #endregion
+    #region Ivy Timing Portfolio Core
+    public abstract class Faber_IvyTiming_Core : Faber_IvyPortfolio_Core
+    {
+        private Dictionary<Instrument, TimeSeries<double>> _monthlyBars = new Dictionary<Instrument, TimeSeries<double>>();
+
+        protected abstract string SAFE_INSTRUMENT { get; }
+        protected override double SCORING_FUNC(Instrument i)
+        {
+            if (i.Nickname == SAFE_INSTRUMENT)
+                return 0.0;
+
+#if true
+            if (!_monthlyBars.ContainsKey(i))
+                _monthlyBars[i] = new TimeSeries<double>();
+
+            if (SimTime[0].Month != NextSimTime.Month)
+                _monthlyBars[i].Value = i.Close[0];
+
+            return (_monthlyBars[i].BarsAvailable > 12 && _monthlyBars[i][0] > _monthlyBars[i].SMA(10)[0])
+                ? 1.0
+                : -1.0;
+#else
+            return i.Close[0] > i.Close.SMA(210)[0]
+                ? 1.0
+                : -1.0;
+#endif
+        }
+    }
+    #endregion
+    #region Ivy Rotation Portfolio Core
+    public abstract class Faber_IvyRotation_Core : Faber_IvyPortfolio_Core
+    {
+        private Dictionary<Instrument, TimeSeries<double>> _monthlyBars = new Dictionary<Instrument, TimeSeries<double>>();
+
+        protected abstract string SAFE_INSTRUMENT { get; }
+        protected override double SCORING_FUNC(Instrument i)
+        {
+            if (i.Nickname == SAFE_INSTRUMENT)
+                return 0.0;
+
+#if true
+            if (!_monthlyBars.ContainsKey(i))
+                _monthlyBars[i] = new TimeSeries<double>();
+
+            if (SimTime[0].Month != NextSimTime.Month)
+                _monthlyBars[i].Value = i.Close[0];
+
+            return (_monthlyBars[i].BarsAvailable > 12 && _monthlyBars[i][0] > _monthlyBars[i].SMA(10)[0])
+                ? (1.0 * (_monthlyBars[i][0] / _monthlyBars[i][3] - 1.0)
+                        + 1.0 * (_monthlyBars[i][0] / _monthlyBars[i][6] - 1.0)
+                        + 1.0 * (_monthlyBars[i][0] / _monthlyBars[i][12] - 1.0))
+                    / 3.0
+                : -1.0;
+#else
+            return i.Close[0] > i.Close.SMA(210)[0]
+                ? (1.0 * (i.Close[0] / i.Close[63] - 1.0)
+                        + 1.0 * (i.Close[0] / i.Close[126] - 1.0)
+                        + 1.0 * (i.Close[0] / i.Close[252] - 1.0))
+                    / 3.0
+                : -1.0;
+#endif
+        }
+    }
+    #endregion
 
     #region Ivy 5 Timing
-    public class Faber_IvyPortfolio_5_Timing : Faber_IvyPortfolio
+    public class Faber_IvyPortfolio_5_Timing : Faber_IvyTiming_Core
     {
         public override string Name => "Faber's Ivy 5 Timing";
 
-        private static readonly string _safeInstrument = "BIL"; // SPDR Barclays 1-3 Month T-Bill ETF
+        protected override string SAFE_INSTRUMENT => "BIL"; // SPDR Barclays 1-3 Month T-Bill ETF
         protected override HashSet<AssetClass> ASSET_CLASSES => new HashSet<AssetClass>
         {
             //--- domestic equity
             new AssetClass { weight = 0.20, numpicks = 1, assets = new List<string> {
                 "VTI", // Vanguard Total Stock Market ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             //--- world equity
             new AssetClass { weight = 0.20, numpicks = 1, assets = new List<string> {
                 "VEU", // Vanguard FTSE All-World ex-US ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             //--- credit
             new AssetClass { weight = 0.20, numpicks = 1, assets = new List<string> {
-                    "BND", // Vanguard Total Bond Market ETF
-                _safeInstrument
+                "BND", // Vanguard Total Bond Market ETF
+                SAFE_INSTRUMENT
             } },
             //--- real estate
             new AssetClass { weight = 0.20, numpicks = 1, assets = new List<string> {
                 "VNQ", // Vanguard REIT Index ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             //--- economic stress
             new AssetClass { weight = 0.20, numpicks = 1, assets = new List<string> {
                 "DBC", // PowerShares DB Commodity Index Tracking
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
         };
-
-        protected override double SCORING_FUNC(Instrument i)
-        {
-            if (i.Nickname == _safeInstrument)
-                return 0.0;
-
-            return i.Close[0] > i.Close.SMA(210)[0]
-                ? 1.0
-                : -1.0;
-        }
     }
     #endregion
     #region Ivy 5 Rotation
-    public class Faber_IvyPortfolio_5_Rotation : Faber_IvyPortfolio
+    public class Faber_IvyPortfolio_5_Rotation : Faber_IvyRotation_Core
     {
         public override string Name => "Faber's Ivy 5 Rotation";
 
-        private static readonly string _safeInstrument = "BIL"; // SPDR Barclays 1-3 Month T-Bill ETF
+        protected override string SAFE_INSTRUMENT => "BIL"; // SPDR Barclays 1-3 Month T-Bill ETF
         protected override HashSet<AssetClass> ASSET_CLASSES => new HashSet<AssetClass>
         {
             new AssetClass { weight = 1.00, numpicks = 3, assets = new List<string> {
@@ -239,99 +308,75 @@ namespace TuringTrader.BooksAndPubs
                 "VNQ", // Vanguard REIT Index ETF
                 //--- economic stress
                 "DBC", // PowerShares DB Commodity Index Tracking
-                _safeInstrument,
-                _safeInstrument,
-                _safeInstrument
-
+                SAFE_INSTRUMENT,
+                SAFE_INSTRUMENT,
+                SAFE_INSTRUMENT
             } },
         };
-
-        protected override double SCORING_FUNC(Instrument i)
-        {
-            if (i.Nickname == _safeInstrument)
-                return 0.0;
-
-            return i.Close[0] > i.Close.SMA(210)[0]
-                ? (4.0 * (i.Close[0] / i.Close[63] - 1.0)
-                        + 2.0 * (i.Close[0] / i.Close[126] - 1.0)
-                        + 1.0 * (i.Close[0] / i.Close[252] - 1.0))
-                    / 3.0
-                : -1.0;
-        }
     }
     #endregion
     #region Ivy 10 Timing
-    public class Faber_IvyPortfolio_10_Timing : Faber_IvyPortfolio
+    public class Faber_IvyPortfolio_10_Timing : Faber_IvyTiming_Core
     {
         public override string Name => "Faber's Ivy 10 Timing";
 
-        private static readonly string _safeInstrument = "BIL"; // SPDR Barclays 1-3 Month T-Bill ETF
+        protected override string SAFE_INSTRUMENT => "BIL"; // SPDR Barclays 1-3 Month T-Bill ETF
         protected override HashSet<AssetClass> ASSET_CLASSES => new HashSet<AssetClass>
         {
             //--- domestic equity
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "VB",  // Vanguard Small Cap ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "VTI", // Vanguard Total Stock Market ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             //--- world equity
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "VWO", // Vanguard Emerging Markets Stock ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "VEU", // Vanguard FTSE All-World ex-US ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             //--- credit
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "BND", // Vanguard Total Bond Market ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "TIP", // iShares Barclays TIPS Bond
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             //--- real estate
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "RWX", // SPDR DJ International Real Estate ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "VNQ", // Vanguard REIT Index ETF
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             //--- economic stress
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "DBC", // 1PowerShares DB Commodity Index Tracking
-                _safeInstrument
+                SAFE_INSTRUMENT
             } },
             new AssetClass { weight = 0.10, numpicks = 1, assets = new List<string> {
                 "GSG", // S&P GSCI(R) Commodity-Indexed Trust
-                _safeInstrument,
+                SAFE_INSTRUMENT,
             } },
         };
-
-        protected override double SCORING_FUNC(Instrument i)
-        {
-            if (i.Nickname == _safeInstrument)
-                return 0.0;
-
-            return i.Close[0] > i.Close.SMA(210)[0]
-                ? 1.0
-                : -1.0;
-        }
     }
     #endregion
     #region Ivy 10 Rotation
-    public class Faber_IvyPortfolio_10_Rotation : Faber_IvyPortfolio
+    public class Faber_IvyPortfolio_10_Rotation : Faber_IvyRotation_Core
     {
         public override string Name => "Faber's Ivy 10 Rotation";
 
-        private static readonly string _safeInstrument = "BIL"; // SPDR Barclays 1-3 Month T-Bill ETF
+        protected override string SAFE_INSTRUMENT => "BIL"; // SPDR Barclays 1-3 Month T-Bill ETF
 
         protected override HashSet<AssetClass> ASSET_CLASSES => new HashSet<AssetClass>
         {
@@ -352,27 +397,13 @@ namespace TuringTrader.BooksAndPubs
                 "DBC", // PowerShares DB Commodity Index Tracking
                 "GSG", // S&P GSCI(R) Commodity-Indexed Trust
                 //---
-                _safeInstrument,
-                _safeInstrument,
-                _safeInstrument,
-                _safeInstrument,
-                _safeInstrument,
-
+                SAFE_INSTRUMENT,
+                SAFE_INSTRUMENT,
+                SAFE_INSTRUMENT,
+                SAFE_INSTRUMENT,
+                SAFE_INSTRUMENT,
             } },
         };
-
-        protected override double SCORING_FUNC(Instrument i)
-        {
-            if (i.Nickname == _safeInstrument)
-                return 0.0;
-
-            return i.Close[0] > i.Close.SMA(210)[0]
-                ? (4.0 * (i.Close[0] / i.Close[63] - 1.0)
-                        + 2.0 * (i.Close[0] / i.Close[126] - 1.0)
-                        + 1.0 * (i.Close[0] / i.Close[252] - 1.0))
-                    / 3.0
-                : -1.0;
-        }
     }
     #endregion
 }
