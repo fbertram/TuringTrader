dadfdff1dd405f730014b32c2a16cfa85f04e604
Felix Bertram <felix@turingtrader.org>
2021-07-08 16:46:26 -0700

    remove symbol cleanup from Yahoo source

--- TuringTrader.Simulator/Simulator/v1/DataSources/DataSourceYahoo.cs
+++ TuringTrader.Simulator/Simulator/v1/DataSources/DataSourceYahoo.cs
@@ -56,7 +56,18 @@ namespace TuringTrader.Simulator
 
             private string convertSymbol(string ticker)
             {
+#if true
+                // new 2021vii01
+                return ticker;
+#else
+                // retired 2021vii01
+                // this code was supposed to help with tickers like BRK.A, which
+                // Yahoo presents as BRK-A. However, it seems that with other stocks,
+                // e.g., IOZ.AX, Yahoo uses the colon. Ultimately, it seems better
+                // to substitute the symbols with a Data Source Descriptor file.
+                // For Yahoo's suffixes see https://au.help.yahoo.com/kb/exchanges-data-providers-yahoo-finance-sln2310.html
                 return ticker.Replace('.', '-');
+#endif
             }
 
             private JObject parsePrices(string raw)
@@ -254,7 +265,7 @@ namespace TuringTrader.Simulator
 
                 return rawMeta;
             }
-            #endregion
+#endregion
 
             //---------- API
             #region public DataSourceYahoo(Dictionary<DataSourceValue, string> info)
