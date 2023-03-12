64c7d4d791904baef2deb2b13c1d229573093d34
Felix Bertram <felix@turingtrader.org>
2023-03-12 14:07:26 -0700

    adjust test after fixing backfills

--- TuringTrader.Tests/v2/T205_Csv.cs
+++ TuringTrader.Tests/v2/T205_Csv.cs
@@ -74,9 +74,8 @@ namespace TuringTrader.SimulatorV2.Tests
 
             Assert.IsTrue(algo.Description.ToLower().Contains("$spxtr.csv"));
             Assert.AreEqual(algo.NumBars, 253);
-            Assert.AreEqual(algo.LastClose / algo.FirstOpen, 196865.1465 / 167008.1405, 1e-3);
-            Assert.AreEqual(algo.HighestHigh / algo.LowestLow, 196865.1465 / 113326.6985, 1e-3); // updated 2023iii03
-            //Assert.IsTrue(Math.Abs(algo.HighestHigh / algo.LowestLow - 197063.5459 / 113326.6985) < 1e-3); // failed 2023iii03
+            Assert.AreEqual(algo.LastClose / algo.FirstOpen, 196865.0 / 167008.0, 1e-3);
+            Assert.AreEqual(algo.HighestHigh / algo.LowestLow, 197064.0 / 113327.0, 1e-3);
         }
     }
 }
