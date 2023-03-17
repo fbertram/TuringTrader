7f083ebe3bc762098c5e39dced74cc3bd15dec43
Felix Bertram <felix@turingtrader.org>
2023-03-17 12:36:46 -0700

    add StandardDeviation, Volatility, and BollingerBands indicators

--- TuringTrader.Simulator/Simulator/v2/Indicators/Volatility.cs
+++ TuringTrader.Simulator/Simulator/v2/Indicators/Volatility.cs
@@ -23,6 +23,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace TuringTrader.SimulatorV2.Indicators
@@ -30,12 +31,50 @@ namespace TuringTrader.SimulatorV2.Indicators
     /// <summary>
     /// Collection of volatility-related indictors.
     /// </summary>
-    public static class Volatility
+    public static class Volatility_
     {
         #region StandardDeviation
         /// <summary>
         /// Calculate historical standard deviation.
         /// </summary>
+        /// <param name="series">input series</param>
+        /// <param name="n">length of calculation window</param>
+        /// <returns>standard deviation time series</returns>
+        public static TimeSeriesFloat StandardDeviation(this TimeSeriesFloat series, int n = 10)
+        {
+            var name = string.Format("{0}.StandardDeviation({1})", series.Name, n);
+
+            return series.Owner.ObjectCache.Fetch(
+                name,
+                () =>
+                {
+                    var data = series.Owner.DataCache.Fetch(
+                        name,
+                        () => Task.Run(() =>
+                        {
+                            var src = series.Data;
+                            var dst = new List<BarType<double>>();
+
+                            for (int idx = 0; idx < src.Count; idx++)
+                            {
+                                // see https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+
+                                var sum = Enumerable.Range(0, n)
+                                    .Sum(t => src[Math.Max(0, idx - t)].Value);
+                                var sum2 = Enumerable.Range(0, n)
+                                    .Sum(t => Math.Pow(src[Math.Max(0, idx - t)].Value, 2.0));
+                                var var = (sum2 - sum * sum / n) / (n - 1);
+
+                                dst.Add(new BarType<double>(
+                                    src[idx].Date, Math.Sqrt(var)));
+                            }
+
+                            return dst;
+                        }));
+
+                    return new TimeSeriesFloat(series.Owner, name, data);
+                });
+        }
         #endregion
         #region SemiDeviation
         #endregion
@@ -43,6 +82,11 @@ namespace TuringTrader.SimulatorV2.Indicators
         /// <summary>
         /// Calculate historical volatility, based on log-returns.
         /// </summary>
+        /// <param name="series"></param>
+        /// <param name="n"></param>
+        /// <returns>volatility time series</returns>
+        public static TimeSeriesFloat Volatility(this TimeSeriesFloat series, int n = 10)
+            => series.LogReturn().StandardDeviation(n);
         #endregion
         #region TrueRange
         /// <summary>
@@ -84,21 +128,147 @@ namespace TuringTrader.SimulatorV2.Indicators
         /// Calculate Averaged True Range, as described here:
         /// <see href="https://en.wikipedia.org/wiki/Average_true_range"/>.
         /// </summary>
+        /// <param name="series"></param>
+        /// <param name="n"></param>
+        /// <returns>average true range time series</returns>
         public static TimeSeriesFloat AverageTrueRange(this TimeSeriesAsset series, int n)
-        {
-            return series.TrueRange().SMA(n);
-        }
+            => series.TrueRange().SMA(n);
+        #endregion
+
+        #region Drawdown
         #endregion
         #region UlcerIndex
         /// <summary>
         /// Calculate Ulcer Index.
         /// </summary>
+        // => series.Drawdown(n).Square().SMA(n).Sqrt();
         #endregion
+
         #region BollingerBands
+        public class BollingerT
+        {
+            /// <summary>
+            /// Middle line, calculated as moving average.
+            /// </summary>
+            public readonly TimeSeriesFloat Middle;
+            /// <summary>
+            /// Upper line, calculated as middle line plus standard deviation.
+            /// </summary>
+            public readonly TimeSeriesFloat Upper;
+            /// <summary>
+            /// Lower line, calculated as middle line minus standard deviation.
+            /// </summary>
+            public readonly TimeSeriesFloat Lower;
+            //public readonly TimeSeriesFloat PercentB;
+
+            /// <summary>
+            /// Create new container for Bollinger Band
+            /// </summary>
+            /// <param name="middle"></param>
+            /// <param name="upper"></param>
+            /// <param name="lower"></param>
+            public BollingerT(TimeSeriesFloat middle, TimeSeriesFloat upper, TimeSeriesFloat lower)
+            {
+                Middle = middle;
+                Upper = upper;
+                Lower = lower;
+            }
+        }
         /// <summary>
         /// Calculate Bollinger Bands, as described here:
-        /// <see href="https://traderhq.com/ultimate-guide-to-bollinger-bands/"/>.
+        /// <see href="https://en.wikipedia.org/wiki/Bollinger_Bands"/>.
         /// </summary>
+        /// <param name="series">input series</param>
+        /// <param name="n">calculation period</param>
+        /// <param name="stdev">width in standard deviations</param>
+        /// <returns>Bollinger Band container of time series</returns>
+        public static BollingerT BollingerBands(this TimeSeriesFloat series, int n = 20, double stdev = 2.0)
+        {
+            var name = string.Format("{0}.BollingerBands({1},{2})", series.Name, n, stdev);
+
+            return series.Owner.ObjectCache.Fetch(
+                name,
+                () =>
+                {
+                    /*var data = series.Owner.DataCache.Fetch(
+                        name,
+                        () => Task.Run(() =>
+                        {
+                            var src = series.Data;
+                            var dst = new List<BarType<double>>();
+                            ...
+                            return dst;
+                        }));*/
+
+                    var std = series.StandardDeviation(n).Mul(stdev);
+                    var mid = series.SMA(n);
+                    var upper = mid.Add(std);
+                    var lower = mid.Sub(std);
+
+                    return new BollingerT(mid, upper, lower);
+                });
+        }
+        #endregion
+
+        #region ValueAtRisk
+        /// <summary>
+        /// Calculate Value-at-Risk at given percentile. The result is 
+        /// expressed as a fraction of the asset's value. A value of 
+        /// 0.1 equates to 10% risk.
+        /// </summary>
+        /// <param name="series">input time series</param>
+        /// <param name="days"># of days to sample</param>
+        /// <param name="percentile">percentile of value at risk. A value of 0.99 represents the 99-th percentile</param>
+        /// <returns>value at risk, expresed as a fraction of the asset value. A value of 0.1 equates to 10% risk</returns>
+        public static TimeSeriesFloat ValueAtRisk(this TimeSeriesAsset series, int days = 21, double percentile = 0.95)
+        {
+            var name = string.Format("{0}.ValueAtRisk({1}, {2}%)", series.Name, days, percentile);
+
+            return series.Owner.ObjectCache.Fetch(
+                name,
+                () =>
+                {
+                    var data = series.Owner.DataCache.Fetch(
+                        name,
+                        () => Task.Run(() =>
+                        {
+                            var RESOLUTION = 1000; // TODO: tie this to percentile
+                            var upsamplingSteps = (int)Math.Ceiling(Math.Log(RESOLUTION) / Math.Log(days));
+
+                            var src = series.Close.LogReturn().Data;
+                            var dst = new List<BarType<double>>();
+
+                            for (int idx = 0; idx < src.Count; idx++)
+                            {
+                                //----- create return distribution with required resolution
+                                var upsampledDistribution = new List<double> { 0.0 };
+                                for (int step = 0; step < upsamplingSteps; step++)
+                                {
+                                    var nextStep = new List<double>();
+                                    foreach (var s1 in upsampledDistribution)
+                                        for (var s2 = 0; s2 < days; s2++)
+                                            nextStep.Add(s1 + src[Math.Max(0, idx - s2)].Value);
+                                    upsampledDistribution = nextStep;
+                                }
+
+                                upsampledDistribution = upsampledDistribution
+                                    .Select(lr => lr / Math.Sqrt(upsamplingSteps))
+                                    .OrderBy(lr => lr)
+                                    .ToList();
+
+                                //----- find value at risk and scale to required period
+                                var distributionAtPercentile = upsampledDistribution[(int)Math.Round(upsampledDistribution.Count * (1.0 - percentile))];
+                                var valueAtRisk = 1.0 - Math.Exp(Math.Sqrt(1.0 / upsamplingSteps) * distributionAtPercentile);
+
+                                dst.Add(new BarType<double>(src[idx].Date, Math.Min(1.0, Math.Max(1e-99, valueAtRisk))));
+                            }
+
+                            return dst;
+                        }));
+
+                    return new TimeSeriesFloat(series.Owner, name, data);
+                });
+        }
         #endregion
     }
 }
--- TuringTrader.Tests/v2/T305_Volatility.cs
+++ TuringTrader.Tests/v2/T305_Volatility.cs
@@ -35,8 +35,26 @@ namespace TuringTrader.SimulatorV2.Tests
     [TestClass]
     public class T305_Volatility
     {
-        #region test TrueRange
-        private class Testbed_TrueRange_V2vsV1 : Algorithm
+        #region test StandardDeviation
+        [TestMethod]
+        public void Test_StandardDeviation()
+        {
+            var algo = new T000_Helpers.DoNothing();
+            algo.StartDate = DateTime.Parse("2022-01-01T16:00-05:00");
+            algo.EndDate = DateTime.Parse("2022-01-31T16:00-05:00");
+            algo.WarmupPeriod = TimeSpan.FromDays(90);
+            algo.CooldownPeriod = TimeSpan.FromDays(0);
+            var asset = algo.Asset("$SPX");
+            var std = asset.Close.StandardDeviation(21).Data
+                .Where(b => b.Date >= algo.StartDate)
+                .ToList();
+
+            Assert.AreEqual(std.Count, 20);
+            Assert.AreEqual(95.5827468751985, std.Average(b => b.Value), 1e-5);
+            Assert.AreEqual(162.52062970246257, std.Max(b => b.Value), 1e-5);
+            Assert.AreEqual(63.713300800076084, std.Min(b => b.Value), 1e-5);
+        }
+        private class Testbed_StandardDeviation_V2vsV1 : Algorithm
         {
             public List<BarType<double>> v1Result;
             public List<BarType<double>> v2Result;
@@ -52,25 +70,94 @@ namespace TuringTrader.SimulatorV2.Tests
                         yield return Simulator.Bar.NewValue(
                             GetType().Name,
                             SimTime[0],
-                            Instruments.First().TrueRange()[0]);
+                            Instruments.First().Close.StandardDeviation(10)[0]);
                 }
             }
             public override void Run()
             {
                 StartDate = DateTime.Parse("2022-01-03T16:00-05:00");
                 EndDate = DateTime.Parse("2022-03-01T16:00-05:00");
-                WarmupPeriod = TimeSpan.FromDays(0);
+                WarmupPeriod = TimeSpan.FromDays(20);
                 CooldownPeriod = TimeSpan.FromDays(0);
 
                 v1Result = Asset(new Testbed_v1()).Close.Data;
-                v2Result = Asset("$SPX").TrueRange().Data;
+                v2Result = Asset("$SPX").Close.StandardDeviation(10).Data;
             }
         }
 
         [TestMethod]
-        public void Test_TrueRange_V2vsV1()
+        public void Test_StandardDeviation_V2vsV1()
         {
-            var algo = new Testbed_TrueRange_V2vsV1();
+            var algo = new Testbed_StandardDeviation_V2vsV1();
+            algo.Run();
+            var v1Result = algo.v1Result;
+            var v2Result = algo.v2Result;
+
+            Assert.AreEqual(v1Result.Count, v2Result.Count);
+
+            for (var i = 0; i < v2Result.Count; i++)
+            {
+                Assert.AreEqual(v1Result[i].Date, v2Result[i].Date);
+                Assert.AreEqual(v1Result[i].Value, v2Result[i].Value, 1e-5);
+            }
+        }
+        #endregion
+        #region test Volatility
+        [TestMethod]
+        public void Test_Volatility()
+        {
+            var algo = new T000_Helpers.DoNothing();
+            algo.StartDate = DateTime.Parse("2022-01-01T16:00-05:00");
+            algo.EndDate = DateTime.Parse("2022-01-31T16:00-05:00");
+            algo.WarmupPeriod = TimeSpan.FromDays(90);
+            algo.CooldownPeriod = TimeSpan.FromDays(0);
+            var asset = algo.Asset("$SPX");
+            var vol = asset.Close.Volatility(21).Data
+                .Where(b => b.Date >= algo.StartDate)
+                .ToList();
+
+            Assert.AreEqual(vol.Count, 20);
+            Assert.AreEqual(0.009779318771068187, vol.Average(b => b.Value), 1e-5);
+            Assert.AreEqual(0.011489691450402354, vol.Max(b => b.Value), 1e-5);
+            Assert.AreEqual(0.008334928184057918, vol.Min(b => b.Value), 1e-5);
+        }
+        #endregion
+        #region test TrueRange
+        private class Testbed_Volatility_V2vsV1 : Algorithm
+        {
+            public List<BarType<double>> v1Result;
+            public List<BarType<double>> v2Result;
+            private class Testbed_v1 : Simulator.Algorithm
+            {
+                public override IEnumerable<Simulator.Bar> Run(DateTime? startTime, DateTime? endTime)
+                {
+                    StartTime = (DateTime)startTime;
+                    EndTime = (DateTime)endTime;
+                    AddDataSource("$SPX");
+
+                    foreach (var st in SimTimes)
+                        yield return Simulator.Bar.NewValue(
+                            GetType().Name,
+                            SimTime[0],
+                            Instruments.First().Close.Volatility(10)[0]);
+                }
+            }
+            public override void Run()
+            {
+                StartDate = DateTime.Parse("2022-01-03T16:00-05:00");
+                EndDate = DateTime.Parse("2022-03-01T16:00-05:00");
+                WarmupPeriod = TimeSpan.FromDays(20);
+                CooldownPeriod = TimeSpan.FromDays(0);
+
+                v1Result = Asset(new Testbed_v1()).Close.Data;
+                v2Result = Asset("$SPX").Close.Volatility(10).Data;
+            }
+        }
+
+        [TestMethod]
+        public void Test_Volatility_V2vsV1()
+        {
+            var algo = new Testbed_Volatility_V2vsV1();
             algo.Run();
             var v1Result = algo.v1Result;
             var v2Result = algo.v2Result;
@@ -131,6 +218,155 @@ namespace TuringTrader.SimulatorV2.Tests
                 Assert.AreEqual(v1Result[i].Date, v2Result[i].Date);
                 Assert.AreEqual(v1Result[i].Value, v2Result[i].Value, 1e-5);
             }
+        }
+        #endregion
+        #region test BollingerBands
+        [TestMethod]
+        public void Test_BollingerBands()
+        {
+            var algo = new T000_Helpers.DoNothing();
+            algo.StartDate = DateTime.Parse("2022-01-01T16:00-05:00");
+            algo.EndDate = DateTime.Parse("2022-01-31T16:00-05:00");
+            algo.WarmupPeriod = TimeSpan.FromDays(90);
+            algo.CooldownPeriod = TimeSpan.FromDays(0);
+            var asset = algo.Asset("$SPX");
+            var bollinger = asset.Close.BollingerBands(20, 1.0);
+
+            var upper = bollinger.Upper.Data
+                .Where(b => b.Date >= algo.StartDate)
+                .ToList();
+            Assert.AreEqual(upper.Count, 20);
+            Assert.AreEqual(4776.819515292047, upper.Average(b => b.Value), 1e-5);
+            Assert.AreEqual(4798.020536843549, upper.Max(b => b.Value), 1e-5);
+            Assert.AreEqual(4728.821722741259, upper.Min(b => b.Value), 1e-5);
+
+            var lower = bollinger.Lower.Data
+                .Where(b => b.Date >= algo.StartDate)
+                .ToList();
+            Assert.AreEqual(lower.Count, 20);
+            Assert.AreEqual(4586.152679532175, lower.Average(b => b.Value), 1e-5);
+            Assert.AreEqual(4652.292302958482, lower.Max(b => b.Value), 1e-5);
+            Assert.AreEqual(4418.809185461867, lower.Min(b => b.Value), 1e-5);
+
+            var middle = bollinger.Middle.Data
+                .Where(b => b.Date >= algo.StartDate)
+                .ToList();
+            Assert.AreEqual(middle.Count, 20);
+            Assert.AreEqual(4681.486097412109, middle.Average(b => b.Value), 1e-5);
+            Assert.AreEqual(4716.387011718751, middle.Max(b => b.Value), 1e-5);
+            Assert.AreEqual(4573.815454101563, middle.Min(b => b.Value), 1e-5);
+        }
+        private class Testbed_BollingerBands_Upper_V2vsV1 : Algorithm
+        {
+            public List<BarType<double>> v1Result;
+            public List<BarType<double>> v2Result;
+            private class Testbed_v1 : Simulator.Algorithm
+            {
+                public override IEnumerable<Simulator.Bar> Run(DateTime? startTime, DateTime? endTime)
+                {
+                    StartTime = (DateTime)startTime;
+                    EndTime = (DateTime)endTime;
+                    AddDataSource("$SPX");
+
+                    foreach (var st in SimTimes)
+                        yield return Simulator.Bar.NewValue(
+                            GetType().Name,
+                            SimTime[0],
+                            Instruments.First().Close.BollingerBands(10, 2.0).Upper[0]);
+                }
+            }
+            public override void Run()
+            {
+                StartDate = DateTime.Parse("2022-01-03T16:00-05:00");
+                EndDate = DateTime.Parse("2022-03-01T16:00-05:00");
+                WarmupPeriod = TimeSpan.FromDays(0);
+                CooldownPeriod = TimeSpan.FromDays(0);
+
+                v1Result = Asset(new Testbed_v1()).Close.Data;
+                v2Result = Asset("$SPX").Close.BollingerBands(10, 2.0).Upper.Data;
+            }
+        }
+        private class Testbed_BollingerBands_Lower_V2vsV1 : Algorithm
+        {
+            public List<BarType<double>> v1Result;
+            public List<BarType<double>> v2Result;
+            private class Testbed_v1 : Simulator.Algorithm
+            {
+                public override IEnumerable<Simulator.Bar> Run(DateTime? startTime, DateTime? endTime)
+                {
+                    StartTime = (DateTime)startTime;
+                    EndTime = (DateTime)endTime;
+                    AddDataSource("$SPX");
+
+                    foreach (var st in SimTimes)
+                        yield return Simulator.Bar.NewValue(
+                            GetType().Name,
+                            SimTime[0],
+                            Instruments.First().Close.BollingerBands(10, 2.0).Lower[0]);
+                }
+            }
+            public override void Run()
+            {
+                StartDate = DateTime.Parse("2022-01-03T16:00-05:00");
+                EndDate = DateTime.Parse("2022-03-01T16:00-05:00");
+                WarmupPeriod = TimeSpan.FromDays(0);
+                CooldownPeriod = TimeSpan.FromDays(0);
+
+                v1Result = Asset(new Testbed_v1()).Close.Data;
+                v2Result = Asset("$SPX").Close.BollingerBands(10, 2.0).Lower.Data;
+            }
+        }
+
+        [TestMethod]
+        public void Test_BollingerBands_V2vsV1()
+        {
+            {
+                var algo = new Testbed_BollingerBands_Upper_V2vsV1();
+                algo.Run();
+                var v1Result = algo.v1Result;
+                var v2Result = algo.v2Result;
+
+                Assert.AreEqual(v1Result.Count, v2Result.Count);
+
+                for (var i = 0; i < v2Result.Count; i++)
+                {
+                    Assert.AreEqual(v1Result[i].Date, v2Result[i].Date);
+                    Assert.AreEqual(v1Result[i].Value, v2Result[i].Value, 1e-5);
+                }
+            }
+            {
+                var algo = new Testbed_BollingerBands_Lower_V2vsV1();
+                algo.Run();
+                var v1Result = algo.v1Result;
+                var v2Result = algo.v2Result;
+
+                Assert.AreEqual(v1Result.Count, v2Result.Count);
+
+                for (var i = 0; i < v2Result.Count; i++)
+                {
+                    Assert.AreEqual(v1Result[i].Date, v2Result[i].Date);
+                    Assert.AreEqual(v1Result[i].Value, v2Result[i].Value, 1e-5);
+                }
+            }
+        }
+        #endregion
+        #region test ValueAtRisk
+        [TestMethod]
+        public void Test_ValueAtRisk()
+        {
+            var algo = new T000_Helpers.DoNothing();
+            algo.StartDate = DateTime.Parse("2022-01-01T16:00-05:00");
+            algo.EndDate = DateTime.Parse("2022-01-31T16:00-05:00");
+            algo.WarmupPeriod = TimeSpan.FromDays(90);
+            algo.CooldownPeriod = TimeSpan.FromDays(0);
+            var asset = algo.Asset("$SPX");
+            var var = asset.ValueAtRisk(21, 0.95).Data
+                .Where(b => b.Date >= algo.StartDate)
+                .ToList();
+            Assert.AreEqual(var.Count, 20);
+            Assert.AreEqual(0.010204094182301643, var.Average(b => b.Value), 1e-5);
+            Assert.AreEqual(0.012947836531692403, var.Max(b => b.Value), 1e-5);
+            Assert.AreEqual(0.00650605717196906, var.Min(b => b.Value), 1e-5);
         }
         #endregion
     }
