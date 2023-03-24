564064ec571cae3a48161b36885f7fb828f078d8
Felix Bertram <felix@turingtrader.org>
2023-03-23 18:09:37 -0700

    update sandbox script

--- More/TuringTrader.Sandbox/Scripts/Install_TuringTrader.cmd
+++ More/TuringTrader.Sandbox/Scripts/Install_TuringTrader.cmd
@@ -1,3 +1,3 @@
-curl -L "https://www.turingtrader.org/downloads/TuringTrader_Setup_v0.14.3.msi" --output C:\users\WDAGUtilityAccount\Desktop\tt.msi
-msiexec /package C:\users\WDAGUtilityAccount\Desktop\tt.msi /quiet
-"c:\Program Files\TuringTrader\Bin\TuringTrader.exe"
+curl -L "https://firebasestorage.googleapis.com/v0/b/turingtrader-org.appspot.com/o/downloads%2FTuringTrader_Setup-16.0.msi?alt=media&token=a48da81e-8d77-4d9e-a585-a59727d2dd81" --output C:\users\WDAGUtilityAccount\Desktop\tt.msi
+cmd /c msiexec /package C:\users\WDAGUtilityAccount\Desktop\tt.msi /quiet
+cmd /c "c:\Program Files\TuringTrader\Bin\TuringTrader.exe"
