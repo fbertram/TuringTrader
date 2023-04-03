46a175edf351cbcb4ffe414bb51a4f7663dbe115
Felix Bertram <felix@turingtrader.org>
2023-04-03 10:57:12 -0700

    simplify strategy using built-in indicators

--- BooksAndPubsV2/Ehlers_RocketScienceForTraders.cs
+++ BooksAndPubsV2/Ehlers_RocketScienceForTraders.cs
@@ -34,86 +34,12 @@
 
 #region libraries
 using System;
-using System.Collections.Generic;
 using TuringTrader.GlueV2;
 using TuringTrader.Optimizer;
 using TuringTrader.SimulatorV2;
 using TuringTrader.SimulatorV2.Assets;
 using TuringTrader.SimulatorV2.Indicators;
-using TuringTrader.SimulatorV2.Porting;
 #endregion
-
-namespace TuringTrader.SimulatorV2.Porting
-{
-    #region helpers to port from TradeStation EasyLanguage
-    public class TradeStation
-    {
-        #region time series
-        public class FloatSeriesManager
-        {
-            private List<FloatSeries> _instances = new List<FloatSeries>();
-            public interface IFloatSeries
-            {
-                double this[int offset] { get; }
-                double Value { get; set; }
-            }
-            private class FloatSeries : IFloatSeries
-            {
-                private List<double> values = new List<double>();
-
-                public FloatSeries(double init)
-                {
-                    values.Add(init);
-                }
-
-                public double this[int offset]
-                    => values[Math.Min(values.Count - 1, offset)];
-
-                public double Value
-                {
-                    get => values[0];
-                    set => values[0] = value;
-                }
-
-                public void Advance()
-                {
-                    values.Insert(0, values[0]);
-
-                    if (values.Count > 256)
-                        values.Remove(values.Count - 1);
-                }
-
-                public static implicit operator double(FloatSeries s) => s[0];
-            }
-
-            public IFloatSeries NewSeries(double init)
-            {
-                var i = new FloatSeries(init);
-                _instances.Add(i);
-
-                return i;
-            }
-
-            public void Advance()
-            {
-                foreach (var i in _instances)
-                    i.Advance();
-            }
-        }
-        #endregion
-        #region trigonometry
-        public static double Sine(double angle) => Math.Sin(Math.PI / 180.0 * angle);
-        public static double Cosine(double angle) => Math.Cos(Math.PI / 180.0 * angle);
-        public static double ArcTangent(double f)
-            // Ehlers' code expects ArcTangent to return angles between
-            // 0 and 180 degrees. In contrast, Math.Atan returns angles
-            // between -Pi/2 and +Pi/2.
-            => 180.0 / Math.PI * Math.Atan(f) + (f < 0.0 ? 180.0 : 0.0);
-            //=> 180.0 / Math.PI * Math.Atan(f);
-        #endregion
-    }
-    #endregion
-}
 
 namespace TuringTrader.BooksAndPubsV2
 {
@@ -190,186 +116,24 @@ namespace TuringTrader.BooksAndPubsV2
                 return bars;
             });
 #endif
-            var Price = asset.High
-                .Add(asset.Low)
-                .Div(2.0);
-
-            var seriesManager = new TradeStation.FloatSeriesManager();
-
-            // the code is taken from the book almost verbatim,
-            // including the variable names.
-            var Smooth = seriesManager.NewSeries(0);
-            var Detrender = seriesManager.NewSeries(0);
-            var I1 = seriesManager.NewSeries(0);
-            var Q1 = seriesManager.NewSeries(0);
-            var jI = seriesManager.NewSeries(0);
-            var jQ = seriesManager.NewSeries(0);
-            var I2 = seriesManager.NewSeries(0);
-            var Q2 = seriesManager.NewSeries(0);
-            var Re = seriesManager.NewSeries(0);
-            var Im = seriesManager.NewSeries(0);
-            var Period = seriesManager.NewSeries(0);
-            var SmoothPeriod = seriesManager.NewSeries(0);
-            var SmoothPrice = seriesManager.NewSeries(0);
-            var DCPeriod = seriesManager.NewSeries(0);
-            var IntPeriod = seriesManager.NewSeries(0);
-            var RealPart = seriesManager.NewSeries(0);
-            var ImagPart = seriesManager.NewSeries(0);
-            var DCPhase = seriesManager.NewSeries(0);
-            var DCSine = seriesManager.NewSeries(0);
-            var LeadSine = seriesManager.NewSeries(0);
-            var ITrend = seriesManager.NewSeries(0);
-            var Trendline = seriesManager.NewSeries(0);
-            var Trend = seriesManager.NewSeries(0);
-            var DaysInTrend = seriesManager.NewSeries(0);
 
             //========== simulation loop ==========
 
             SimLoop(() =>
             {
-                // advance all series
-                seriesManager.Advance();
+                var Price = asset.High
+                    .Add(asset.Low)
+                    .Div(2.0);
+                var SmoothPrice = Price.WMA(4);
 
-                // note the complicated feedback through the
-                // period value, which makes porting to 
-                // TuringTrader's indicators tricky
-
-                Smooth.Value = (4.0 * Price[0] + 3.0 * Price[1]
-                    + 2.0 * Price[2] + Price[3])
-                    / 10.0;
-
-                Detrender.Value = (0.0962 * Smooth[0] + 0.5769 * Smooth[2]
-                    - 0.5769 * Smooth[4] - 0.0962 * Smooth[6])
-                    * (0.075 * Period[1] + 0.54);
-
-                //--- compute in-phase and quadrature components
-                Q1.Value = (0.0962 * Detrender[0] + 0.5769 * Detrender[2]
-                    - 0.5769 * Detrender[4] - 0.0962 * Detrender[6])
-                    * (0.075 * Period[1] + 0.54);
-
-                I1.Value = Detrender[3];
-
-                //--- advance the phase of I1 and Q1 by 90 degrees
-                jI.Value = (0.0962 * I1[0] + 0.5769 * I1[2]
-                    - 0.5769 * I1[4] - 0.0962 * I1[6])
-                    * (0.075 * Period[1] + 0.54);
-
-                jQ.Value = (0.0962 * Q1[0] + 0.5769 * Q1[2]
-                    - 0.5769 * Q1[4] - 0.0962 * Q1[6])
-                    * (0.075 * Period[1] + 0.54);
-
-                //--- phasor addition for 3-bar averaging
-                I2.Value = I1[0] - jQ[0];
-                Q2.Value = Q1[0] + jI[0];
-
-                //--- smooth the i and q components before applying the discriminator
-                I2.Value = 0.2 * I2[0] + 0.8 * I2[1];
-                Q2.Value = 0.2 * Q2[0] + 0.8 * Q2[1];
-
-                //--- homodyne discriminator
-                Re.Value = I2[0] * I2[1] + Q2[0] * Q2[1];
-                Im.Value = I2[0] * Q2[1] - Q2[0] * I2[1];
-
-                Re.Value = 0.2 * Re[0] + 0.8 * Re[1];
-                Im.Value = 0.2 * Im[0] + 0.8 * Im[1];
-
-                if (Im[0] != 0.0 && Re[0] != 0.0) Period.Value = 360.0 / TradeStation.ArcTangent(Im[0] / Re[0]);
-                if (Period[0] > 1.5 * Period[1]) Period.Value = 1.5 * Period[1];
-                if (Period[0] < 0.67 * Period[1]) Period.Value = 0.67 * Period[1];
-                if (Period[0] < 6.0) Period.Value = 6.0;
-                if (Period[0] > 50.0) Period.Value = 50.0;
-
-                Period.Value = 0.2 * Period[0] + 0.8 * Period[1];
-                SmoothPeriod.Value = 0.33 * Period[0] + 0.67 * SmoothPeriod[1];
-
-                //--- compute dominant cycle phase
-                SmoothPrice.Value = (4.0 * Price[0] + 3.0 * Price[1]
-                    + 2.0 * Price[2] + Price[3]) / 10.0;
-
-                DCPeriod.Value = Math.Floor(SmoothPeriod[0] + 0.5);
-
-                RealPart.Value = 0.0;
-                ImagPart.Value = 0.0;
-                for (var count = 0; count < DCPeriod[0]; count++)
-                {
-                    RealPart.Value = RealPart[0]
-                        + TradeStation.Cosine(360.0 * count / DCPeriod[0]) * SmoothPrice[count];
-                    ImagPart.Value = ImagPart[0]
-                        + TradeStation.Sine(360.0 * count / DCPeriod[0]) * SmoothPrice[count];
-                }
-
-                if (Math.Abs(RealPart[0]) > 0.0) DCPhase.Value = TradeStation.ArcTangent(ImagPart[0] / RealPart[0]);
-                if (Math.Abs(RealPart[0]) <= 0.001) DCPhase.Value = 90.0 * Math.Sign(ImagPart[0]);
-                DCPhase.Value = DCPhase[0] + 90.0;
-                var phase1 = DCPhase[0];
-
-                //--- compensate for one bar lag of weighted moving average
-                DCPhase.Value = DCPhase[0] + 360.0 / SmoothPeriod[0];
-                var phase2 = DCPhase[0];
-
-                if (ImagPart[0] < 0.0) DCPhase.Value = DCPhase[0] + 180.0;
-                if (DCPhase[0] > 315.0) DCPhase.Value = DCPhase[0] - 360.0;
-                var phase3 = DCPhase[0];
-
-                //--- compute the Sine and LeadSine indicators
-                DCSine.Value = TradeStation.Sine(DCPhase[0]);
-                LeadSine.Value = TradeStation.Sine(DCPhase[0] + 45.0);
-
-                //--- compute trendline as simple average over the measured dominant cycle period
-                ITrend.Value = 0.0;
-#if true
-                // variant w/ configurable CycPart parameter (see fig 12.4.),
-                // presented by Ehlers as an additional improvement
-                IntPeriod.Value = Math.Floor(CYC_PART / 100.0 * SmoothPeriod[0] + 0.5);
-                for (var count = 0; count < IntPeriod[0]; count++)
-                    ITrend.Value = ITrend[0] + Price[count];
-                if (DCPeriod[0] > 0.0) ITrend.Value = ITrend[0] / IntPeriod[0];
-#else
-                // orignal variant (see fig 12.1.), equivalent to CycPart = 1.0
-                for (var count = 0; count < DCPeriod[0]; count++)
-                    ITrend.Value = ITrend[0] + Price[count];
-                if (DCPeriod[0] > 0.0) ITrend.Value = ITrend[0] / DCPeriod[0];
-#endif
-
-                Trendline.Value = (4.0 * ITrend[0] + 3.0 * ITrend[1]
-                    + 2.0 * ITrend[2] + ITrend[3])
-                    / 10.0;
-
-                // if CurrentBar < 12 then Trendline = Price;
-
-                //--- assume trend mode
-                Trend.Value = 1.0;
-
-                //--- measure days in trend from last crossing of the sinewave indicator lines
-                if (DCSine[0] > LeadSine[0] && DCSine[1] <= LeadSine[1]
-                    || DCSine[0] < LeadSine[0] && DCSine[1] >= LeadSine[1])
-                {
-                    DaysInTrend.Value = 0.0;
-                    Trend.Value = 0.0;
-                }
-
-                DaysInTrend.Value = DaysInTrend[0] + 1.0;
-                if (DaysInTrend[0] < 0.5 * SmoothPeriod[0]) Trend.Value = 0.0;
-
-                //--- cycle mode if delta phase is +/-50% of dominant cycle change of phase
-                if (SmoothPeriod[0] != 0.0
-                    && DCPhase[0] - DCPhase[1] > 0.67 * 360.0 / SmoothPeriod[0]
-                    && DCPhase[0] - DCPhase[1] < 1.5 * 360.0 / SmoothPeriod[0])
-                    Trend.Value = 0.0;
-
-                //--- declare a trend mode if the smoothprice is more than 1.5% from the trendline
-                if (Math.Abs((SmoothPrice[0] - Trendline[0]) / Trendline[0]) >= 0.015)
-                    Trend.Value = 1.0;
+                // all of Ehlers's complex calculations are provided
+                // through a family of easy-to-use indicators
+                var Sine = Price.SinewaveIndicator().Sine;
+                var LeadSine = Price.SinewaveIndicator().LeadSine;
+                var Trendline = Price.InstantaneousTrendline();
+                var Trend = Price.MarketMode();
 
                 //--- trade trend mode
-                // if Trend = 1 then begin
-                //     if Trend[1] = 0 then begin
-                //         if MarketPosition = -1 and SmoothPrice >= Trendline then buy;
-                //         if MarketPosition = 1 and SmoothPrice < Trendline then sell;
-                //     end;
-                //     if SmoothPrice crosses over Trendline then buy;
-                //     if SmoothPrice crosses under Trendline then sell;
-                // end;
                 double targetAllocation;
                 if (Trend[0] == 1.0 && TRADE_TREND != 0)
                     targetAllocation = SmoothPrice[0] > Trendline[0]
@@ -377,12 +141,8 @@ namespace TuringTrader.BooksAndPubsV2
                         : (ALLOW_SHORT != 0 ? -1.0 : 0.0);
 
                 //--- trade cycle mode
-                // if Trend = 0 then begin
-                //     if LeadSine crosses over  DCSine then buy;
-                //     if LeadSine crosses under DCSinde then sell;
-                // end;
                 else if (Trend[0] == 0.0 && TRADE_CYCLE != 0)
-                    targetAllocation = LeadSine[0] > DCSine[0]
+                    targetAllocation = LeadSine[0] > Sine[0]
                         ? (ALLOW_LONG != 0 ? 1.0 : 0.0)
                         : (ALLOW_SHORT != 0 ? -1.0 : 0.0);
 
@@ -403,30 +163,19 @@ namespace TuringTrader.BooksAndPubsV2
 
 #if true
                     // optional charts of internal signals
-                    Plotter.SelectChart("Dominant Cycle", "Date");
+                    Plotter.SelectChart("Sinewave Indicator", "Date");
                     Plotter.SetX(SimDate);
                     Plotter.Plot("Price", 25.0 * Math.Log(Price[0] / Price[(DateTime)StartDate]));
-                    Plotter.Plot("DC Period", DCPeriod[0]);
+                    Plotter.Plot("Sine", 10.0 * Sine[0] + 20.0);
+                    Plotter.Plot("Lead Sine", 10.0 * LeadSine[0] + 20.0);
+                    Plotter.Plot("Trending", Trend[0] != 0.0 ? 10.0 : 0.0);
 
-                    Plotter.SelectChart("Market Mode", "Date");
-                    Plotter.SetX(SimDate);
-                    Plotter.Plot("Price", 25.0 * Math.Log(Price[0] / Price[(DateTime)StartDate]));
-                    Plotter.Plot("Trending", Trend[0] != 0.0 ? 25.0 : 0.0);
-                    Plotter.Plot("Days in Trend", Math.Min(50.0, DaysInTrend[0]));
-
-                    Plotter.SelectChart("Trend Trading", "Date");
+                    Plotter.SelectChart("Instantaneous Trendline", "Date");
                     Plotter.SetX(SimDate);
                     Plotter.Plot("Price", 25.0 * Math.Log(Price[0] / Price[(DateTime)StartDate]));
                     Plotter.Plot("Smoothed Price", 25.0 * Math.Log(SmoothPrice[0] / Price[(DateTime)StartDate]));
                     Plotter.Plot("Trendline", 25.0 * Math.Log(Trendline[0] / Price[(DateTime)StartDate]));
-                    Plotter.Plot("Trending", Trend[0] != 0.0 ? 25.0 : 0.0);
-
-                    Plotter.SelectChart("Cycle Trading", "Date");
-                    Plotter.SetX(SimDate);
-                    Plotter.Plot("Price", 25.0 * Math.Log(Price[0] / Price[(DateTime)StartDate]));
-                    Plotter.Plot("DC Sine", 10.0 * DCSine[0]);
-                    Plotter.Plot("Lead Sine", 10.0 * LeadSine[0]);
-                    Plotter.Plot("Trending", Trend[0] != 0.0 ? 25.0 : 0.0);
+                    Plotter.Plot("Trending", Trend[0] != 0.0 ? 10.0 : 0.0);
 #endif
                 }
             });
