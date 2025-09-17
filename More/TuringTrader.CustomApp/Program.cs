cc0185fa0ba86cafb9ea120065919b8b3924d6af
Liz Bertram <girlnamedliz@gmail.com>
2025-09-17 15:32:25 -0700

    update copyright

--- More/TuringTrader.CustomApp/Program.cs
+++ More/TuringTrader.CustomApp/Program.cs
@@ -2,9 +2,9 @@
 // Project:     TuringTrader, custom application
 // Name:        Program.cs
 // Description: Custom application based on TuringTrader
-// History:     2020iv08, FUB, created
+// History:     2020iv08, EFB, created
 //------------------------------------------------------------------------------
-// Copyright:   (c) 2011-2023, Bertram Enterprises LLC dba TuringTrader.
+// Copyright:   (c) 2011-2025, Bertram Enterprises LLC dba TuringTrader.
 //              https://www.turingtrader.org
 // License:     This file is part of TuringTrader, an open-source backtesting
 //              engine/ trading simulator.
